d14ae6337ea8f45e8f2824115e9a3aa875ddddda
CTSuwan [Chonmapoohm Thamsuwan] <ct.suwan@gmail.com>
2026-03-03 16:10:34 +0700

    Add Set X solutions (Verified 100%)

---
+++ X-n148-k46.sol
@@ -0,0 +1,47 @@
+Route 1: 66 148 142 52
+Route 2: 70 39 10 51
+Route 3: 134 145 132 103
+Route 4: 42 79 46
+Route 5: 133 98 29
+Route 6: 26 30 64 25 12
+Route 7: 31 61 40
+Route 8: 2 60
+Route 9: 44 144 77 96 88
+Route 10: 122 141
+Route 11: 17 75 109
+Route 12: 6 72 87
+Route 13: 16 91 117
+Route 14: 20 65 95 139 32
+Route 15: 4 13 118
+Route 16: 125 57 84
+Route 17: 53 102 49 7 126 38
+Route 18: 22 59
+Route 19: 73 94
+Route 20: 5 115 54 127
+Route 21: 101 116 136 9
+Route 22: 27 131
+Route 23: 23 56 28
+Route 24: 41 106
+Route 25: 112 124 147 93
+Route 26: 47 90
+Route 27: 48 74
+Route 28: 24 43 100
+Route 29: 85 123
+Route 30: 81 120 104
+Route 31: 108 119
+Route 32: 99 82 121 21
+Route 33: 128 78 110 63
+Route 34: 111 86 135
+Route 35: 138 83 137
+Route 36: 18 113 3
+Route 37: 35 140
+Route 38: 80 92 129
+Route 39: 68 62 8 58
+Route 40: 19 89
+Route 41: 45 76
+Route 42: 105 71 36 143
+Route 43: 33 114
+Route 44: 50 146 67 14 37 34 97
+Route 45: 55 11 69 15
+Route 46: 107 130
+Cost 43167.85
